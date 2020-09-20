6238eadd893bd47149eebe313461bbc49ae4fa5e
Grant Watson <watsongo@etsu.edu>
2020-09-20 14:44:04 -0400

    Revert "Relocated API files"
    This reverts commit d8a3671bd75f12c0ed942002b8b3ba1aaeb90ac3.

---
+++ WebApplication1/WebApplication1/Models/MetricContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MetricsAPI.Models
+{
+    public class MetricContext : DbContext
+    {
+        public MetricContext(DbContextOptions<MetricContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SiteData> SiteData { get; set; }
+    }
+}
