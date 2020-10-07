8e0bc7554d027d0559404ad1a1d62bc8cdf6d192
Grant Watson <watsongo@etsu.edu>
2020-10-07 17:32:09 -0400

    User select queries
    User can select all or select specifc records

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,10 +10,47 @@ namespace ConsoleApp2
         static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            String answer, switchAnswer;
             var client = new HttpClient();
             var response = await client.GetAsync("http://localhost:5001/api/site");
             var message = await response.Content.ReadAsStringAsync();
+            Console.WriteLine("\nCurrently in db:");
             Console.WriteLine(message);
+            do
+            {
+                Console.Write("What records would you like to see (get all, get one)? ");
+                answer = Console.ReadLine();
+                switch(answer)
+                {
+                    case "get all":
+                        response = await client.GetAsync("http://localhost:5001/api/site");
+                        message = await response.Content.ReadAsStringAsync();
+                        break;
+                    case "get one":
+                        Console.WriteLine("Select the Monitor_ID to get: ");
+                        switchAnswer = Console.ReadLine();
+                        response = await client.GetAsync($"http://localhost:5001/api/site/{switchAnswer}");
+                        message = await response.Content.ReadAsStringAsync();
+                        break;
+                        /*
+                    case "delete":
+                        Console.WriteLine("Select the Monitor_ID to delete: ");
+                        switchAnswer = Console.ReadLine();
+                        response = await client.GetAsync($"http://localhost:5001/api/site/delete/{switchAnswer}");
+                        message = await response.Content.ReadAsStringAsync();
+                        break;
+                    case "update":
+                        response = await client.GetAsync("http://localhost:5001/api/sit/update");
+                        message = await response.Content.ReadAsStringAsync();
+                        break;
+                        */
+                    default:
+                        answer = "Leave";
+                        break;
+                }
+                Console.WriteLine(message);
+            }
+            while ((!answer.Equals("Leave", StringComparison.OrdinalIgnoreCase)));
         }
     }
 }
