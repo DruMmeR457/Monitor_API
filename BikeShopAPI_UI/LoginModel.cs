95bc99691d42f5c94c8c1ed10236cf065d5abb8a
Jackson Brooks <brooksjt@etsu.edu>
2020-10-14 13:20:12 -0400

    Added UI (Hopefully this is going to the UI branch as I intended)

---
+++ BikeShopAPI_UI/LoginModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BikeShopAPI_UI
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Input { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public LoginModel()
+        {
+            Description = "User Name";
+        }
+
+    }
+}
