943b7e987bf247c0d82042ff0134fd0387ebb8c5
Jackson Brooks <brooksjt@etsu.edu>
2020-11-10 17:56:39 -0500

    no message

--- BikeShopAPI_UI/Startup.cs
+++ BikeShopAPI_UI/Startup.cs
@@ -48,6 +48,7 @@ namespace BikeShopAPI_UI
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<Service>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
